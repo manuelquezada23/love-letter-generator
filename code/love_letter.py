952d2964539175301c29b7ebfd0fb8ea35f10380
Manuel Quezada <manuel_quezada@brown.edu>
2022-12-04 14:34:59 -0500

    more progress

--- code/love_letter.py
+++ code/love_letter.py
@@ -1,12 +1,13 @@
 import numpy as np
 import pickle
+from model import LoveLetterGeneratorModel
 
 def main(args):
     DATA_PATH = './data/'
     with open(DATA_PATH, 'rb') as data_file:
         data_dict = pickle.load(data_file)
     
-    model = LoveLetterGenerator()
+    model = LoveLetterGeneratorModel()
 
 if __name__ == '__main__':
     main()
