8e35040ee709f4d628ed5f618600e46ef05bae32
Manuel Quezada <manuel_quezada@brown.edu>
2022-12-04 17:05:06 -0500

    refactoring into love_letter

--- code/love_letter.py
+++ code/love_letter.py
@@ -1,13 +1,43 @@
 import numpy as np
 import pickle
 from model import LoveLetterGeneratorModel
+from poet_class import Poet
+import re
+import os  
 
-def main(args):
-    DATA_PATH = './data/'
-    with open(DATA_PATH, 'rb') as data_file:
-        data_dict = pickle.load(data_file)
+POEMS_PATH = 'past_poems/'
+poet_name = input('What is the name of the poet?\n\n')
+lover = input('Who is the letter dedicated to?\n\n')
+poet = Poet(poet_name, lover)
+
+def convert_to_filename(value):
+    value = re.sub('[^\w\s-]', '', value).strip().lower()
+    value = re.sub('[-\s]+', '-', value)
+    return value
+
+def main():
+    # DATA_PATH = './data/'
+    # with open(DATA_PATH, 'rb') as data_file:
+    #     data_dict = pickle.load(data_file)
     
-    model = LoveLetterGeneratorModel()
+    # model = LoveLetterGeneratorModel()
+
+    while True:
+        try:
+            seed = input('Feeling inspired? How do you want to start the poem\n\n')
+            print(poet.write_poem(seed))
+            # write poem
+            poem = poet.write_poem(seed)
+            # write poem as .txt in putpus
+            filename = convert_to_filename(poem.split('\n')[0])
+            with open(POEMS_PATH + filename + '.txt', "w") as text_file:
+                text_file.write(poem)
+            # another poem?
+            repeat = input('Another poem? (Y/N) ')
+            if repeat.lower() == 'n':
+                break
+        except KeyboardInterrupt:
+            break
 
 if __name__ == '__main__':
     main()
